dcc307416c3e7fbcf7795d27645e1fd1aef2beaa
DibsTHEgreat <pateliyandivya@gmail.com>
2025-04-09 11:24:17 -0600

    adding some observations about LL

--- Data Structures/LinkedList.py
+++ Data Structures/LinkedList.py
@@ -1,3 +1,13 @@
 ## This file contains my observations and notes about Linked Lists
 ## I have also created my own Linked List Class to understand the DS
 
+## Observations
+
+## Linked Lists(LL) do not have indexes.
+## LL is a type of list that is in a contiguous place in memory, as in, all elements are all right next to each other in memory.
+## With a LL all the nodes are going to be spread all over the place.
+
+## Also with a LL there is a variabled called head which points to the first node in a LL
+##   head                tail
+##    |                    |
+##   (1) -> (2) -> (3) -> (4) -> None (null)
