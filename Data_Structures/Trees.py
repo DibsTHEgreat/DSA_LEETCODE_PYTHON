6cf24e9d31f0cf26f455dd879301670b212917cf
DibsTHEgreat <pateliyandivya@gmail.com>
2025-04-15 10:24:03 -0600

    comments about Binary Search Trees

--- Data_Structures/Trees.py
+++ Data_Structures/Trees.py
@@ -64,3 +64,33 @@
 #           / \   / \
 #          o   o o   o
 # Since two nodes share the same parent, those two nodes can also be considered siblings. Every node can only have one parent.
+
+# What is a binary search tree?
+# With a binary search tree, if you are to add a node, the number of the node will determine the placement. For example,
+# if the number is greater than the parent it goes onto the right, if the number is less than the parent it goes on the left.\
+#                   (47)
+#                       \
+#                       (76)
+# For example, if we were to add Node 52 to the BST, we would have to do this:
+#                   (47) <-- Start by comparing with the top node, since 52 is greater than 47 we go to the right side
+#                       \
+#                       (76) <-- Since there is already a node here, we compare the 52 to the 76. Since 52 < 76, we put 52 on the left side.
+#                       /
+#                     (52)
+# For example, if we were to add Node 21 to the BST, we would have to do this:
+#                   (47) <-- Start by comparing with the top node, since 21 is less than 47 we go to the left side
+#                  /    \
+#                (21)  (76)
+#                       /
+#                     (52)
+# For example, if we were to add Node 82 to the BST, we would have to do this:
+#                   (47) <-- Start by comparing with the top node, since 82 is greater than 47 we go to the right side
+#                  /    \
+#                (21)  (76) <-- Now we compare 76 to the new node 82, since 82 is greater than 76 we go to the right side
+#                       /  \
+#                     (52) (82)
+# Some things to point out in the BST:
+# If you take any node in the BST, all nodes below the main node (or any given node) on the right side are going to be
+# greater than the chosen node. Likewise applies for when all nodes below the main node on the left side will be
+# less than the chosen node.
+
